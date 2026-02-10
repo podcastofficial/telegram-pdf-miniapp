b3c4eb41583176ae0356a2d53fc0f22649f1f6e0
podcastofficial <podcastofficial4@gmail.com>
2026-02-10 12:22:43 +0530

    Refactor merge function into sendAction
    Removed merge function and added sendAction function to handle actions.

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
 
-function merge(){
-  const input = document.getElementById("files");
-  let form = new FormData();
-  for (let f of input.files) {
-    form.append("files", f);
-  }
+function sendAction(action) {
 
-  fetch("https://YOUR_VPS_IP:5000/merge", {
-    method: "POST",
-    body: form
-  })
-  .then(res => res.blob())
-  .then(() => tg.showAlert("✅ PDF merged & saved"));
+  // 🔥 Show Adsgram Ad
+  showAd(() => {
+    tg.sendData(action);
+  });
+
 }
