727a9f9dfdc39002f2afa327442b9712b4fff5d5
podcastofficial <podcastofficial4@gmail.com>
2026-02-13 12:14:26 +0530

    Delete list.test.js

--- list.test.js
+++
@@ -1,9 +0,0 @@
-const listCommand = require("../../commands/list");
-
-it("responds to /list command", async () => {
-	expect.assertions(1);
-	const response = await listCommand(bot)(bot.message(), {
-		groups: { Provider: "" },
-	});
-	expect(response).toBe(undefined);
-});
